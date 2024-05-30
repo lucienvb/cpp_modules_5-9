70cc0568d2f1b35a75a04093ca60bd6d88b05a2a
Lucien Van Bussel <lvan-bus@f1r6s8.codam.nl>
2024-05-30 15:52:52 +0200

    now with better test cases

--- cpp_module_05/ex02/src/main.cpp
+++ cpp_module_05/ex02/src/main.cpp
@@ -4,44 +4,60 @@
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 
-int main() {
-    Bureaucrat lucien("lucien", 2);
-    Bureaucrat maurice("maurice", 73);
-    Bureaucrat jerome("jerome", 146);
+void	testShrub(){
+	std::cout << "* test ShruberyCreationForm *" << std::endl <<std::endl;
+	try{
+		Bureaucrat lucien("lucien", 1);
+		ShrubberyCreationForm form("ShrubShrub");
+		form.execute(lucien);
+	}
+	catch(const std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+}
 
-    ShrubberyCreationForm shrub("tree");
-    RobotomyRequestForm rob("robot");
-    PresidentialPardonForm pres("pres");
-    try {
-        lucien.executeForm(shrub);
-        lucien.signAForm(shrub);
-        Bureaucrat bur;
-        shrub.beSigned(bur);
-        shrub.execute(bur);
-        lucien.executeForm(rob);
-        lucien.executeForm(pres);
-    }
-    catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+void	testRob(){
+	std::cout << "* test RobotomyRequest *" << std::endl;
+	try{
+		Bureaucrat maurice("maurice", 71);
+		RobotomyRequestForm form("form");
+		for (int i = 0; i < 5; i++){
+			form.execute(maurice);
+			std::cout << std::endl;
+		}
+	}
+	catch(const std::exception& e){
+		std::cerr << e.what() << std::endl;
+		}
+	std::cout << std::endl;
+}
 
-    std::cout << "\n";
-    try {
-        maurice.executeForm(shrub);
-        maurice.executeForm(rob);
-        maurice.executeForm(pres);
-    }
-    catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    
-    std::cout << "\n";
-    try {
-        jerome.executeForm(shrub);
-        jerome.executeForm(rob);
-        jerome.executeForm(pres);
-    }
-    catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+void	testPres(void){
+	std::cout << "* test PresidentialPardonForm *" << std::endl;
+
+	try{
+	PresidentialPardonForm form("''Form of Pardon''" );
+	Bureaucrat jerome("jerome", 151);
+	form.execute(jerome);
+	std::cout << std::endl;
+	}
+	catch(const std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+	Bureaucrat ghislaine("ghislaine", 4);
+	PresidentialPardonForm pardon_form("''Form of Pardon''" );
+	ghislaine.executeForm(pardon_form);
+	std::cout << std::endl;
+}
+
+int	main(void){
+	testPres();
+	std::cout << "\n";
+	testRob();
+	std::cout << "\n";
+	testShrub();
+	return 0;
 }
