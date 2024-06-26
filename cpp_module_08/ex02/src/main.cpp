5eb5bb86e17027f756372754a096970fa7ab4748
Lucien Van Bussel <lvan-bus@f1r3s9.codam.nl>
2024-06-26 15:56:58 +0200

    added test cases for the reverse iterator

--- cpp_module_08/ex02/src/main.cpp
+++ cpp_module_08/ex02/src/main.cpp
@@ -23,17 +23,14 @@
 //     ++it;
 //     }
 //     std::stack<int> s(mstack);
-//     return 0
+//     return 0;
 // }
-
-#include <iostream>
-#include "MutantStack.hpp"
 
 int main() {
     MutantStack<int> mstack;
 
-    mstack.push(5);
-    mstack.push(17);
+    mstack.push(-9999);
+    mstack.push(33);
 
     std::cout << "Top element: " << mstack.top() << std::endl;
 
@@ -41,21 +38,23 @@ int main() {
 
     std::cout << "Size: " << mstack.size() << std::endl;
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
+    for (int i = 0; i < 10; i++) {
+        mstack.push(i * (i + 3));
+    }
 
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    std::cout << "Top element: " << mstack.top() << std::endl;
+    std::cout << "Size: " << mstack.size() << std::endl;
 
-    ++it;
-    --it;
+    MutantStack<int>::const_reverse_iterator it = mstack.rbegin();
+    MutantStack<int>::const_reverse_iterator ite = mstack.rend();
+
+    it++;
+    it--;
 
     std::cout << "MutantStack elements:" << std::endl;
     while (it != ite) {
         std::cout << *it << std::endl;
-        ++it;
+        it++;
     }
 
     std::stack<int> s(mstack);
